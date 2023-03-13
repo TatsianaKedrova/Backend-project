778b72605be53766256e0ba25cbb2b4ab0db5351
TatsianaKedrova <Baletrot@gmail.com>
2023-03-13 12:51:55 +0200

    initial commit for the folder with all the initial necessary packages including TS

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  let helloMessage = "Great and creative Sherif!";
+  res.send(helloMessage);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
