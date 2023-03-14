5ff631b131a72d901a91f33024e1e1944ae93c73
TatsianaKedrova <Baletrot@gmail.com>
2023-03-14 14:13:05 +0200

    added get request with URI param

--- dist/routers/products-router.js
+++ dist/routers/products-router.js
@@ -9,10 +9,16 @@ exports.productsRouter.get("/", (req, res) => {
     const products = products_repository_1.productsRepository.getProducts();
     res.send(products);
 });
+exports.productsRouter.get("/:id", (req, res) => {
+    const chosenProduct = products_repository_1.productsRepository
+        .getProducts()
+        .find((product) => product.id === req.params.id);
+    res.status(http_status_codes_1.StatusCodes.OK).json(chosenProduct);
+});
 exports.productsRouter.post("/", (req, res) => {
     const newProduct = products_repository_1.productsRepository.createProduct(req.body.title);
     if (newProduct) {
-        res.status(201).send(newProduct);
+        res.status(http_status_codes_1.StatusCodes.CREATED).send(newProduct);
     }
     else {
         res.sendStatus(http_status_codes_1.StatusCodes.NOT_FOUND);
--- src/routers/products-router.ts
+++ src/routers/products-router.ts
@@ -9,10 +9,17 @@ productsRouter.get("/", (req: Request, res: Response) => {
   res.send(products);
 });
 
+productsRouter.get("/:id", (req: Request, res: Response) => {
+  const chosenProduct = productsRepository
+    .getProducts()
+    .find((product) => product.id === req.params.id);
+  res.status(StatusCodes.OK).json(chosenProduct);
+});
+
 productsRouter.post("/", (req: Request, res: Response) => {
   const newProduct = productsRepository.createProduct(req.body.title);
   if (newProduct) {
-    res.status(201).send(newProduct);
+    res.status(StatusCodes.CREATED).send(newProduct);
   } else {
     res.sendStatus(StatusCodes.NOT_FOUND);
   }
